02fd325e095defcf5f029eec350fde9612aacb03
Rose123345 <awukurose58@gmail.com>
2026-06-21 17:18:09 +0000

    employee_record_system

---
+++ community-contributions/employee_record_system/employee_system.py
@@ -0,0 +1,10 @@
+print("="*35)
+print("Employee Record System")
+print("="*35)
+
+name = input("Enter employee name: ")
+salary = float(input("Enter employee salary: "))
+department = input("Enter employee department: ")
+
+print(f"{name} works in the {department} department and earns a salary of ${salary:.2f}.")
+print("Monthly salary: GHS", monthly_salary)
