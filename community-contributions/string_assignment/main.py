12b097323a0255bc10d1e08c044d5443681cad55
semacksonjerry <semacksonjerry2014@gmail.com>
2026-07-01 10:45:32 -0700

    Added string assignmnet

---
+++ community-contributions/string_assignment/main.py
@@ -0,0 +1,10 @@
+print("=== String Assignmnet ===")
+
+full_name = input("Enter your full name:")
+
+print("Total number of characters:", len(full_name))
+print("First 3 character:", full_name[:3])
+print("Last 3 character:", full_name[-3:])
+print("Uppercase:", full_name.upper())
+print("Lower:", full_name.lower())
+print("Title case:", full_name.title())
